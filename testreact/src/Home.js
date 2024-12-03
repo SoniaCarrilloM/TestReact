7bd0ff470625525146ae871bf8aa88abd098a8d7
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-12-03 16:02:27 +0100

    added list

--- testreact/src/Home.js
+++ testreact/src/Home.js
@@ -1,6 +1,21 @@
-const Home = () => {
-  const handleClick = () => {};
+import { useState } from "react";
 
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My favourite website",
+      body: "lorem ipsum...",
+      author: "Mario",
+      id: 1,
+    },
+    { title: "Best ever!", body: "lorem ipsum...", author: "Yoshi", id: 2 },
+    {
+      title: "Unbeatable",
+      body: "lorem ipsum...",
+      author: "Pippa",
+      id: 3,
+    },
+  ]);
   return (
     <div className="Home">
       <h3
@@ -8,9 +23,15 @@ const Home = () => {
           color: "blue",
         }}
       >
-        Practice your English skills
-        <button onClick={handleClick}>Click Me</button>
+        Reviews
       </h3>
+
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
